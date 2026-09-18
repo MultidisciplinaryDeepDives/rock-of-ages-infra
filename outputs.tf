b348a0358d6fb1f70d2619ce15bd4234a0f32c22
Interdisciplinary_Adventures <144870031+MultidisciplinaryDeepDives@users.noreply.github.com>
2026-09-17 19:47:16 -0500

    Initial Commit

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,10 @@ output "cloudfront_domain" {
   value       = aws_cloudfront_distribution.frontend.domain_name
   description = "Domain name of the CloudFront distribution"
 }
+
+
+output "bucket_name" {
+  value       = aws_s3_bucket.frontend.id  
+  description = "My client S3 bucket name"
+}
+
