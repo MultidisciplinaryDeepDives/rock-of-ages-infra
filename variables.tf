48dc1221c0294a66f342d27113b2f3404aec14ab
Interdisciplinary_Adventures <144870031+MultidisciplinaryDeepDives@users.noreply.github.com>
2026-09-23 21:23:01 -0500

    created iam file, modified variables file

--- variables.tf
+++ variables.tf
@@ -10,3 +10,9 @@ variable "bucket_name" {
   description = "Name of the S3 bucket (must be globally unique)"
   type        = string
 }
+
+variable "github_org" {
+  type        = string
+  description = "GitHub organization or username"
+  default     = "MultidisciplinaryDeepDives" #Change to your github username
+}
